47a44340433029c5db2856e1eb0e49a98c8c39f5
Way, No <noway@2ch.hk>
2016-12-31 17:31:31 +1300

    setup.py from pyenet is being ran

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import distutils
 from setuptools import setup, find_packages, Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
@@ -22,10 +24,28 @@ for name in names:
     ext_modules.append(Extension(name, ['%s.pyx' % name.replace('.', '/')],
         language = 'c++', include_dirs=['pyspades'], **extra))
 
+from distutils.command.build import build as _build
+
+
+class build(_build):
+    def run(self):
+        print "** running enet"
+        distutils.core.run_setup(os.path.join(sys.path[0], "enet/setup.py"), ['build_ext', '--inplace'])
+        print "** ran enet"
+        _build.run(self)
+        # self.execute(_run_build_tables, (self.install_lib,),
+        #              msg="Build the lexing/parsing tables")
+
+pyspades_ext_modules = cythonize(ext_modules) 
+
+from pprint import pprint
+print "-- ext modules"
+print pyspades_ext_modules
+pprint(pyspades_ext_modules[0].__dict__)
 
 setup(
     name = 'pysnip',
-    packages = ['pysnip', 'pyspades', 'pysnip.feature_server'],
+    packages = ['pysnip', 'pyspades', 'pyspades.enet', 'pysnip.feature_server'],
     version = '0.0.0',
     description = 'Open-source server implementation for Ace of Spades',
     author = 'Matpow2, Stackoverflow',
@@ -46,6 +66,10 @@ setup(
         	'pysnip=pysnip.feature_server.run:main'
     	],
     },
-    package_dir = {'pysnip': '', 'pyspades': 'pyspades'},
-    ext_modules = cythonize(ext_modules)
+    package_dir = {'pysnip': '', 'pyspades': 'pyspades', 'pyspades.enet': 'enet'},
+    package_data = {'pyspades.enet': ["enet.so"]},
+    # ext_package = "pyspades",
+    ext_modules = pyspades_ext_modules ,
+    cmdclass = {'build': build},
+
 )
